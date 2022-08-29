2aa481db53b69d06c0d6292aa135b3f447da49ca
Veysel <veyselanik@sabanciuniv.edu>
2022-08-30 01:06:35 +0300

    post/get requests

--- server.js
+++ server.js
@@ -14,6 +14,30 @@ const friends = [
         name: 'Steve Jobs'
     }
 ];
+
+app.use((req,res,next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req,res) => {
+    if (!req.body.name){
+        return res.status(400).json({
+            error: 'Missing friend name'
+        }); 
+    }
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    };
+    friends.push(newFriend);
+    res.json(newFriend);
+});
+
 app.get('/friends', (req,res) =>{
     res.json(friends);
 });
