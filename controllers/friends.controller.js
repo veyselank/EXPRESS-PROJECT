15990a81b7ae8b49c76f9f040175f0bc9eb48acc
Veysel <veyselanik@sabanciuniv.edu>
2022-08-30 23:37:23 +0300

    friends controller setup

---
+++ controllers/friends.controller.js
@@ -0,0 +1,36 @@
+function postFriend(req,res)  {
+    if (!req.body.name){
+        return res.status(400).json({
+            error: 'Missing friend name'
+        }); 
+    }
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    };
+    friends.push(newFriend);
+    res.json(newFriend);
+}
+
+function getFriends(req,res){
+    res.json(friends);
+}
+
+function getFriend(req,res){
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    }
+    else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
+}
+
+module.exports = {
+    getFriends,
+    postFriend,
+    getFriend,
+};
--- server.js
+++ server.js
@@ -27,36 +27,11 @@ app.use((req,res,next) => {
 
 app.use(express.json());
 
-app.post('/friends', (req,res) => {
-    if (!req.body.name){
-        return res.status(400).json({
-            error: 'Missing friend name'
-        }); 
-    }
-    const newFriend = {
-        name: req.body.name,
-        id: friends.length
-    };
-    friends.push(newFriend);
-    res.json(newFriend);
-});
+app.post('/friends', );
 
-app.get('/friends', (req,res) =>{
-    res.json(friends);
-});
+app.get('/friends', );
 
-app.get('/friends/:friendId', (req,res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend);
-    }
-    else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        });
-    }
-});
+app.get('/friends/:friendId', );
 
 app.get('/messages', messagesController.getMessage);
 
